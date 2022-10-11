2283e10bf9a1594ec691d795e5ddff5dc00ac65d
TLulo <112913190+TLulo@users.noreply.github.com>
2022-10-11 02:02:18 -0300

    Add files via upload

---
+++ C/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool resc,rese;
+int x, y, z, a, b, c , d, e;
+int main(void)
+{
+    printf("Ingrese el valor de x:");
+    scanf("%d", &x);
+
+    printf("Ingrese el valor de y:");
+    scanf("%d", &y);
+    
+    printf("Ingrese el valor de z:");
+    scanf("%d", &z);
+
+    a = x + y + 1;
+    b = z * z + y * 45 -15 * x;
+    c = y-2 == (x * 3 + 1) % 5;
+    d = y / 2 * x;
+    e = y < x * z;
+    resc = c;
+    rese = e;
+
+    printf("El resultado de la primera ecuacion es: %d\n",a);
+    printf("El resultado de la segunda ecuacion es: %d\n",b);
+    printf("El resultado de la tercera ecuacion es: %d\n",resc);
+    printf("El resultado de la cuarta ecuacion es: %d\n",d);
+    printf("El resultado de la quinta ecuacion es: %d\n",rese);    
+
+
+}
