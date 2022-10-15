eac29a91ed474bd9ac12cc0b04f73b49cc4c526f
TLulo <112913190+TLulo@users.noreply.github.com>
2022-10-15 02:10:13 -0300

    Add files via upload

---
+++ C/ejer6d.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void imprimeHola(void){
+    printf("hola\n");
+}
+void imprimeChau(void){
+    printf("chau\n");
+}
+
+int main(void){
+    imprimeHola();
+    imprimeHola();
+    imprimeChau();
+    imprimeChau();
+}
