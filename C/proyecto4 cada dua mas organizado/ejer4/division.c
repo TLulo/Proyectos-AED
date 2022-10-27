e31ecc94fcd3f780c8a7514977033305f41bb449
MILF (man i lov famaf) <86092120+MikeyTR@users.noreply.github.com>
2022-10-27 18:38:57 -0300

    EPICO
    QE

--- C/proyecto4 cada dua mas organizado/ejer4/division.c
+++ C/proyecto4 cada dua mas organizado/ejer4/division.c
@@ -37,3 +37,43 @@ int main(void){
     
 }
 
+
+//-------------------------------------------ESTA VEZ FUE SIN ROBARTE-------------------------------
+
+#include <stdio.h>
+
+int pedirEntero(void){
+    int i;
+    printf("Introduzca el numero:\n");
+    scanf("%d",&i);
+    return i;
+}
+
+struct div_t {
+int cociente;
+int resto;
+};
+
+struct div_t division(int x, int y){
+    struct div_t res;
+    res.cociente= x/y;
+    res.resto= x%y;
+    
+    return res;
+}
+
+
+int main() {
+    int x,y;
+    struct div_t div;
+    x = pedirEntero();
+    y = pedirEntero();
+    while (y==0){
+        printf("El entero que ingreso no es valido\n");
+        pedirEntero();
+    }
+    div = division(x,y);
+    printf("El Cociente es: %d\nEl resto es: %d.\n",div.cociente, div.resto);
+    
+}
+
