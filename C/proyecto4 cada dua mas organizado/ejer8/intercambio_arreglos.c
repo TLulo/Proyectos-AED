c839b4aa55d52c2d58799d7e3cd7532e5f7ade73
MILF (man i lov famaf) <86092120+MikeyTR@users.noreply.github.com>
2022-10-27 20:37:31 -0300

    Update intercambio_arreglos.c

--- C/proyecto4 cada dua mas organizado/ejer8/intercambio_arreglos.c
+++ C/proyecto4 cada dua mas organizado/ejer8/intercambio_arreglos.c
@@ -47,3 +47,81 @@ int main(void){
     pedirArreglo(a,n);
     intercambiar(a,n,i-1,j-1);
 }
+
+
+
+
+
+
+
+
+//#######################################APRUEBA DE ERRORES ESTE ES MAS OPE#############################
+
+#include <stdio.h>
+#include<stdbool.h>
+
+int pedirLargo(void){
+    int i;
+    printf("Introduzca el largo del arreglo:\n");
+    scanf("%d",&i);
+    return i;
+}
+
+int pedirEntero(void){
+    int i;
+    printf("Introduzca un numero:\n");
+    scanf("%d",&i);
+    return i;
+}
+
+void pedirArreglo(int a[], int n_max) {
+    int i;
+    i = 0;
+    while (i < n_max) {
+        printf("Ingrese el valor del arreglo en la posicion %d:", i);
+        scanf("%d",&a[i]);
+        i=i+1;
+    }
+}
+
+void imprimeArreglo(int a[], int n_max){
+    int i;
+    i=0;
+    while(i < n_max) {
+        printf("El numero %d esta en la posicion %d;\n", a[i],i);
+        i = i+1;
+    }
+}
+
+void intercambiar(int a[], int tam, int i, int j) {
+    int iaux;
+    iaux = a[i];
+    a[i] = a[j];
+    a[j] = iaux;
+    imprimeArreglo(a,tam);
+}
+
+int main (void) {
+    int k,n,i,j;
+    n = pedirLargo();
+    int a[n];
+    pedirArreglo(a,n);
+    imprimeArreglo(a,n);
+    printf("Ingrese las posiciones a intercambiar.\n");
+    i = pedirEntero();
+    j = pedirEntero();
+    k = 1;
+    while ( k != 0){
+    if (i >= n) {
+        printf("Ingrese una posicion valida para i: \n");
+        i = pedirEntero();
+    } else if (j >= n) {
+        printf("Ingrese una posicion valida para j: \n");
+        j = pedirEntero();
+    } else {
+        i = i;
+        j = j;
+        k = 0;
+    }}
+    intercambiar(a,n,i,j);
+}
