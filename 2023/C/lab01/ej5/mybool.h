09e96bf987f8455dc4cb42b6e5bdbe1a301254c4
TLulo <112913190+TLulo@users.noreply.github.com>
2024-03-15 17:22:51 -0300

    Add files via upload

---
+++ 2023/C/lab01/ej5/mybool.h
@@ -0,0 +1,9 @@
+#ifndef _MYBOOL_H_
+#define _MYBOOL_H_
+
+#define true 1
+#define false 0
+
+typedef int mybool;
+
+#endif
---
+++ 2023/C/lab01/ej6/array_helpers.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include "array_helpers.h"
+
+unsigned int array_from_file(int array[],
+                             unsigned int max_size,
+                             const char *filepath) {
+    FILE*archivo;
+    archivo = fopen(filepath,"r");
+
+    fscanf(archivo , "%u", &max_size);
+
+    for (unsigned int i = 0u; i < max_size; ++i)
+    {
+        fscanf(archivo , "%d" , &array[i]);
+    }
+
+    fclose(archivo);
+    return max_size;
+}
+
+void array_dump(int a[], unsigned int length) {
+
+
+    for (unsigned int i = 0; i < length; ++i)
+    {
+        printf("% d ",a[i]);
+    }
+    printf("\n");
+}
+
+bool array_is_sorted(int array[], unsigned int length) {
+    unsigned int i;
+    for (i=0; i < length - 1; i++) {
+        if (array[i] > array[i + 1]) {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+void array_swap(int a[], unsigned int i, unsigned int j) {
+    int tmp;
+
+    tmp = a[i];
+    a[i] = a[j];
+    a[j] = tmp;
+    
+}
