c13bacb307443275c3d5d6c4254c5ce929cd0335
TLulo <112913190+TLulo@users.noreply.github.com>
2024-04-21 01:03:54 -0300

    Create long_words.cpp

---
+++ long_words.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  string s;
+  string abreviature;
+  int n;
+  cin >> s;
+  n = s.length();
+  abreviature = s[0]+n+s[n];
+  cout << abreviature
+}
